a8feb2e7d484987ded6135d540178c188a78bbe2
Wali Rahman <wali.rahman89@yahoo.com>
2018-10-18 12:37:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
+
 
 function showResults(json) {
   //use this function to display the results from forking via the API
